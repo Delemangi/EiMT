232dc3d7417a4330598bcaf00198d091ff5f0509
Delemangi <milev.stefan@gmail.com>
2023-03-17 17:59:41 +0100

    Initial commit

---
+++ backend/src/main/java/mk/finki/ukim/emt/eshop/EShopApplication.java
@@ -0,0 +1,11 @@
+package mk.finki.ukim.emt.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShopApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EShopApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/mk/finki/ukim/emt/eshop/EShopApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.finki.ukim.emt.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
