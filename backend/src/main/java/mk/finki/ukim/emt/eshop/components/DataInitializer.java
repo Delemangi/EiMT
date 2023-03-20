cb3d5285438f49db3d21b86acb91c058116bbd2e
Delemangi <milev.stefan@gmail.com>
2023-03-20 03:04:16 +0100

    Add more data for testing

--- backend/src/main/java/mk/finki/ukim/emt/eshop/components/DataInitializer.java
+++ backend/src/main/java/mk/finki/ukim/emt/eshop/components/DataInitializer.java
@@ -28,7 +28,7 @@ public class DataInitializer {
             return;
         }
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 1; i < 31; i++) {
             create(i);
         }
     }
